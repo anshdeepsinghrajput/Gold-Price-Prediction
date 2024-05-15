601fd913e7159610add7bff9b7aba7454a9eb0f9
Anshdeep Singh Rajput <anshurajput1852001@gmail.com>
2024-05-15 23:47:05 +0530

    remove tts

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import joblib
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 import sklearn

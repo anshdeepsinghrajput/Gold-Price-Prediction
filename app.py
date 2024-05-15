9550482067ee9164bd5fa04cbb22e4737d4302d8
Anshdeep Singh Rajput <anshurajput1852001@gmail.com>
2024-05-15 23:39:02 +0530

    add sklearn again

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
+import sklearn
 
 st.title("Gold Price Predictor")
 
